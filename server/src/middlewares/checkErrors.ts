479dc0103affc1f3c272a658c4a21a09cae88960
darkmalk <sa.vergaramunoz@gmail.com>
2026-01-10 05:17:13 -0300

    fix: enhance error handling for Zod validation errors in checkErrors middleware

--- server/src/middlewares/checkErrors.ts
+++ server/src/middlewares/checkErrors.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from 'express'
 import { HttpCodes } from '../types.js'
+import { ZodError } from 'zod'
 
 const checkErrors = (
   err: any,
@@ -8,6 +9,19 @@ const checkErrors = (
   _next: NextFunction
 ) => {
   const status = err.statusCode || HttpCodes.INTERNAL_SERVER_ERROR
+
+  if (err instanceof ZodError) {
+    const errors: Record<string, string> = {}
+
+    err.issues.forEach(value => {
+      errors[value.path.join('')] = value.message
+    })
+
+    return res.status(HttpCodes.BAD_REQUEST).json({
+      message: 'has occurred an error with the inputs in request body',
+      errors
+    })
+  }
 
   return res.status(status).json({
     message: err.message || 'Internal error'
