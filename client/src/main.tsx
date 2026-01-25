120573d9d1aa78724538ff232031d5c1502adfe1
darkmalk <sa.vergaramunoz@gmail.com>
2026-01-25 19:38:52 -0300

    feat: add routing with react-router and restructure App component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import { RouterProvider } from 'react-router'
 
 import './assets/global.css'
+import { routes } from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>
 )
